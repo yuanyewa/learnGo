aac1817cf7cac361c01bd9f7120f3199963b8d0f
yuanye wang <yuanyewang@yuanyes-MacBook-Pro.local>
2018-10-07 13:00:27 -0700

    add gorilla handler middleware

--- 08_mux/mymux.go
+++ 08_mux/mymux.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -15,6 +17,7 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/profile/{user}", handleUser)
-	http.Handle("/", r)
+	// http.Handle("/", r)
+	http.Handle("/", handlers.LoggingHandler(os.Stdout, r))
 	http.ListenAndServe(":8080", nil)
 }
