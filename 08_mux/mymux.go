4707d110d48e019ee09b237ed4e2292fa4324d2d
yuanye wang <yuanyewang@yuanyes-MacBook-Pro.local>
2018-10-08 22:21:02 -0700

    add context

--- 08_mux/mymux.go
+++ 08_mux/mymux.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,13 +14,16 @@ import (
 func mymiddle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Println("this is me")
+		r = r.WithContext(context.WithValue(r.Context(), "foo", "bar"))
 		next(w, r)
 	}
 }
+
 func handleUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	// w.Write([]byte(vars["user"]))
 	fmt.Fprintf(w, vars["user"])
+	fmt.Fprintf(w, r.Context().Value("foo").(string))
 }
 func main() {
 	r := mux.NewRouter()
