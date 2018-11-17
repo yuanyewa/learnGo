244c8e9bb97a247ae4fbbdaefe00a67dd74e6a9c
yuanye wang <yuanye.wang@intel.com>
2018-11-16 17:15:51 -0800

    function types

---
+++ 00_mine/functype.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+)
+
+func add1(x int) int {
+	return x + 1
+}
+func applyFn(f func (int) int, x int) int {
+	return f(x)
+}
+func makefunc(x int, y int) func (int) int {
+	return func(z int) int {
+		return z + x + y
+	}
+}
+
+func prtSlice(x []int) func () {
+	return func () {
+		fmt.Println(x)
+	}
+}
+func vara(x ...int) {
+	s := 0
+	for _, v := range x {
+		s += v
+	}
+	fmt.Println(s)
+}
+func main() {
+	var c func (int) int
+	c = add1 // variable as function
+	fmt.Println(c(1))
+	fmt.Println(applyFn(add1, 1)) // function as input
+	fmt.Println(applyFn(func (x int) int {return x + 100}, 1)) // anonymous function
+	f := makefunc(10,100) // function as output
+	fmt.Println(f(1))
+	sls := []int{1,2,3}
+	val := 100
+	defer fmt.Println("Deferred print: ", sls, val) // value to deferred function is fixed immediately; if value is a pointer, the content may still change
+	val ++
+	p := prtSlice(sls)
+	p()
+	sls[0] = 1000
+	p() // note here, printed [0] is 1000, meanning function p has access to sls parameter through it's constructing function. p is not fixed by the time it's created.
+	vara([]int{3,4,5}...) // varadic function: in definition: var ...Type; when call: slice...
+}
